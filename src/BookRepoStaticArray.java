bd6eeba10625280a296f5074b823602623ad3ce6
Yutta_u <davidchastokolovrf@gmail.com>
2023-01-25 22:37:39 +0500

    add try-catch add throsw

--- src/BookRepoStaticArray.java
+++ src/BookRepoStaticArray.java
@@ -1,4 +1,4 @@
-public class BookRepoStaticArray implements BookRepo {
+public class BookRepoStaticArray implements ArrayList {
     private static int index = 0;
     private static final Book[] books = new Book[10];
 
@@ -9,7 +9,7 @@ public class BookRepoStaticArray implements BookRepo {
         if (get(book.getName()) != null)
             return false;
         books[index++] = book;
-        return true; // добавить исключение
+        return true;
     }
 
     @Override
@@ -21,23 +21,21 @@ public class BookRepoStaticArray implements BookRepo {
     }
 
     public void update(int id, Book book) {
-    //    books[id] = book;
+        books[id] = book;
     }
-
 
     public Book[] getAll() {
         return books;
     }
 
     public Book get(int id) {
-        return books[id]; // добавить исключение
+        return books[id];
     }
+
 
     public Book delete() {
         return books[index] = null;
     }
 
-    //TODO метод изменения какого то параметра книги. +
     //TODO выводить ошибки. нету книги, перебор с кол-вом, выкидывать исключения
-    //TODO метод удаления книг с перестановкой в левую часть всех последующих книг по массиву -+
 }
